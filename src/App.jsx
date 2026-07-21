706e7678247045dc5ab3444c3d1a7393f98e9c37
Dewangga Tirta Kencana <dewanggatirtakencana25@gmail.com>
2026-07-22 00:20:22 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router";
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import NotFound from "./pages/Notfound";
+
+function App() {
+  return (
+    <nav>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+        </Routes>
+        <ul>
+          <li>
+            <Link to="dashboard">Dashboard</Link>
+          </li>
+
+          <li>
+            <Link to="login">Login</Link>
+          </li>
+        </ul>
+    </nav>
+    
+  );
+}
+
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NotFound(){
+    return(
+        <h1>Maaf, Halaman tidak di temukan !</h1>
+    )
+}
+
+export default NotFound;
